52b8c532abe551ae4e8cd69f387661fbf3f14bc6
joseph-winner <muhanguzijoseph2021@gmail.com>
2024-01-08 20:48:46 +0300

    Added Pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque nisi delectus nesciunt blanditiis neque. Vero dolore a mollitia tempora molestias autem ratione impedit sunt, pariatur qui blanditiis aliquid enim in, temporibus delectus minima fugit non reiciendis quae eligendi doloremque, dolorum laborum iusto itaque? Deserunt provident fuga deleniti fugit, voluptatum cum.
+    </div>
+  )
+}
